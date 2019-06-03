78a4a7e99b19b552207cafba4699cc450fa97fce
sheva <shevchuksanya983@gmail.com>
2019-06-04 00:59:48 +0300

    make one selectListener for each select; Polar option does not work yet

--- FirstTask/firstTask.js
+++ FirstTask/firstTask.js
@@ -1,17 +1,17 @@
-const regions = document.querySelector('#task1 select[name="region"]');
-const subregions = document.querySelector('#task1 select[name="subregion"]');
-const countries = document.querySelector('#task1 select[name="country"');
 const countryInfo = document.querySelector('#task1 #country-info');
 const loadingIcon = document.querySelector('#wrapper');
-
-function displaySelects(){
-    if(subregions.hasChildNodes()){
-      
-    }
+const urls = {
+    'region': './subregions.json',
+    'subregion' : 'https://restcountries.eu/rest/v2/subregion/',
+    'country' : 'https://restcountries.eu/rest/v2/name/'
 }
+
+const selects = document.querySelectorAll('#task1 select');
+    const selectsNames = Array.from(document.querySelectorAll('#task1 select'))
+                            .map((element)=>element.name);
+
 function hideSelects(currentSelect, selects, selectsNames){
     let index = selectsNames.indexOf(currentSelect);
-    console.log(index);
     for(let i = index+1; i < selects.length; i++){
         selects[i].innerHTML = "";
         // selects[i].className = "hidden";
@@ -30,110 +30,55 @@ function createDefaultOption(currentSelect){
     return selectedOption; 
 }
 
-function selectListener(){
-
+function createOptions(data, field=null){
+    let options;
+    if(field){
+        options = data[field].map(element => {
+            let option = document.createElement('option');
+            option.value = element;
+            option.text = element;
+            return option;
+        });
+    } else {
+        options = data.map(element => {
+            let option = document.createElement('option');
+            option.value = element.name;
+            option.text = element.name;
+            return option;
+        });
+    }
+    return options;
 }
 
-function main(){
-    const selects = document.querySelectorAll('#task1 select');
-    const selectsNames = Array.from(document.querySelectorAll('#task1 select'))
-                            .map((element)=>element.name);
-    
-    regions.addEventListener('change', function(){
-        
-        loadingIcon.style.display = "block";
-        let region = this.value;
-        let nameOfSelector = selectsNames[selectsNames.indexOf(this.name)+1];
-        hideSelects(this.name, selects, selectsNames);
-        setTimeout(function(){
-            fetch('./subregions.json')
-                .then(res => res.json())
-                .then(data => {
-                    loadingIcon.style.display = "none";
-            
-                    subregions.appendChild(createDefaultOption(nameOfSelector));
-                
-                    if(!data[region][0]) {
-                        subregions.style.display = "none";
-                        countries.style.display = "inline-block";
-                        subregions.dispatchEvent(new Event('change'));
-                    } else {
-                        subregions.style.display = "inline-block";
-                        //countries.style.display = "none";
-                    }
-    
-                    data[region].map(subregion => {
-                        let option = document.createElement('option');
-                        option.value = subregion;
-                        option.text = subregion;
-                        subregions.appendChild(option);
-                    })        
-                })
-                .catch(e => {
-                    console.log(e);
-                })
-        }, 1500); 
-    });
-    
-    subregions.addEventListener('change', function(){
-        loadingIcon.style.display = "block";
-        let nameOfSelector = selectsNames[selectsNames.indexOf(this.name)+1];
-        if(regions.value == "polar"){
-            loadingIcon.style.display = "none";
-            let selectedOption = document.createElement('option');
-            selectedOption.disabled = true;
-            selectedOption.hidden = true;
-            selectedOption.text = "Choose country"; 
-            selectedOption.selected = true;
-            countries.appendChild(selectedOption);
-            
-            let option = document.createElement('option');
-            option.value = 'Antarctica'
-            option.text = 'Antarctica';
-            countries.appendChild(option);
-            return;
-        }
-    
-        let subregion = this.value;
-        let url = `https://restcountries.eu/rest/v2/subregion/${subregion}`;
-    
-        
-        hideSelects(this.name, selects, selectsNames);
-    
-        setTimeout(function(){
-            fetch(url)
-                .then(res => res.json())
-                .then(data => {
-                    loadingIcon.style.display = "none";
-                    countries.style.display = "inline-block";
-                   
-                    countries.appendChild(createDefaultOption(nameOfSelector));
-    
-                    data.map(country => {
-                        let option = document.createElement('option');
-                        option.value = country.name;
-                        option.text = country.name;
-                        countries.appendChild(option);
-                    })
-                })
-                .catch(e => {
-                    console.log(e);
-                })
-        }, 1500);
+function addOptions(selector, ...options){
+    console.log(selector);
+    options.forEach(option => {
+        console.log(option);
+        selector.appendChild(option);
     })
-    
-    countries.addEventListener('change', function(){
-        loadingIcon.style.display = "block";
-        let country = this.value;
-        let url = `https://restcountries.eu/rest/v2/name/${country}`;
-       
-        hideSelects(this.name, selects, selectsNames)
-    
-        setTimeout(function(){
-            fetch(url)
-                .then(res => res.json())
-                .then(data => {
-                    loadingIcon.style.display = "none";
+}
+
+function selectListener(){
+
+    loadingIcon.style.display = "block";
+
+    let valueOfCurrentSelector = this.value;
+    let indexOfCurrentSelector = selectsNames.indexOf(this.name);
+    let nameOfCurrentSelector = selectsNames[selectsNames.indexOf(this.name)];
+    let nameOfNextSelector = selectsNames[selectsNames.indexOf(this.name)+1];
+    let url;
+    if(nameOfCurrentSelector==='region'){
+        url = urls[nameOfCurrentSelector];
+    } else {
+        url = `${urls[nameOfCurrentSelector]}${valueOfCurrentSelector}`;
+    }
+    hideSelects(this.name, selects, selectsNames);
+    setTimeout(function(){
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                loadingIcon.style.display = "none";
+                if(nameOfCurrentSelector==='country'){
                     data.map(item => {
                         let {alpha2Code, capital, population, nativeName, flag} = item;
                         let element = document.createElement('p');
@@ -150,13 +95,40 @@ function main(){
     
                         countryInfo.appendChild(flagImage);
                     })
-                })
-                .catch(e => {
-                    console.log(e);
-                })
-        }, 1500);
-    })
+                } else{
+                    selects[indexOfCurrentSelector+1]
+                    .appendChild(createDefaultOption(nameOfNextSelector));
+            
+                // if(!data[valueOfCurrentSelector][0]) {
+                //     subregions.style.display = "none";
+                //     countries.style.display = "inline-block";
+                //     subregions.dispatchEvent(new Event('change'));
+                // } else {
+                //     selects[indexOfCurrentSelector+1].style.display = "inline-block";
+                //     //countries.style.display = "none";
+                // }
+                selects[indexOfCurrentSelector+1].style.display = "inline-block";
+                let field;
+                if(nameOfCurrentSelector==='region'){
+                    field = valueOfCurrentSelector;
+                } else {
+                    field = null;
+                }
+                let options = createOptions(data, field);
+                addOptions(selects[selectsNames.indexOf(nameOfNextSelector)], ...options);
+                }
+                      
+            })
+            .catch(e => {
+                console.log(e);
+            })
+    }, 1500); 
 }
+
+function main(){
+    selects.forEach(select => select.addEventListener('change', selectListener));
+}
+
 main();
 
 
