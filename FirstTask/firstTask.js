0f7be333e30d502e009322d4d91da26884828d8b
sheva <shevchuksanya983@gmail.com>
2019-06-04 02:43:37 +0300

    create getUrl function

--- FirstTask/firstTask.js
+++ FirstTask/firstTask.js
@@ -1,24 +1,22 @@
-const countryInfo = document.querySelector('#task1 #country-info');
-const loadingIcon = document.querySelector('#wrapper');
+const countryInfo = document.querySelector('#country-info');
 const urls = {
     'region': './subregions.json',
     'subregion' : 'https://restcountries.eu/rest/v2/subregion/',
     'country' : 'https://restcountries.eu/rest/v2/name/'
 }
-
 const selects = document.querySelectorAll('#task1 select');
-    const selectsNames = Array.from(document.querySelectorAll('#task1 select'))
-                            .map((element)=>element.name);
+const selectsNames = Array.from(selects).map((element)=>element.name);
 
-function hideSelects(currentSelect, selects, selectsNames){
-    let index = selectsNames.indexOf(currentSelect);
-    for(let i = index+1; i < selects.length; i++){
+function hideNextSelects(indexOfCurrentSelector){
+    for(let i = indexOfCurrentSelector+1; i < selects.length; i++){
         selects[i].innerHTML = "";
-        // selects[i].className = "hidden";
-        // selects[i].classList.add('hidden');
         selects[i].style.display = 'none';
     }
     countryInfo.innerHTML = "";
+}
+
+function showNextSelect(index){
+    selects[index].style.display = "inline-block";
 }
 
 function createDefaultOption(currentSelect){
@@ -51,76 +49,86 @@ function createOptions(data, field=null){
 }
 
 function addOptions(selector, ...options){
-    console.log(selector);
     options.forEach(option => {
-        console.log(option);
         selector.appendChild(option);
     })
 }
 
+function showLoadingIcon(){
+    document.querySelector('#wrapper').style.display = "block";
+}
+
+function hideLoadingIcon(){
+    document.querySelector('#wrapper').style.display = "none";
+}
+
+function showCountryInfo(data){
+    data.map(item => {
+        let {alpha2Code, capital, population, nativeName, flag} = item;
+        let element = document.createElement('p');
+
+        element.innerText = 
+            `Country code: ${alpha2Code}\nCapital: ${capital}
+            Population: ${population}\nNative name: ${nativeName}`;
+        countryInfo.appendChild(element);
+
+        let flagImage = document.createElement('img');
+        flagImage.display="block";
+        flagImage.style.width="40%";
+        flagImage.src = flag;
+
+        countryInfo.appendChild(flagImage);
+    })
+}
+
+function getUrl(nameOfSelector, valueOfSelector){
+    if(valueOfSelector === 'polar'){
+        return urls['country'] + 'Antarctica';
+    } else if(urls[nameOfSelector].includes('.json')){
+        return urls[nameOfSelector];
+    } else {
+        return urls[nameOfSelector] + valueOfSelector;
+    }
+}
+
 function selectListener(){
-
-    loadingIcon.style.display = "block";
-
     let valueOfCurrentSelector = this.value;
     let indexOfCurrentSelector = selectsNames.indexOf(this.name);
+    let indexOfNextSelector = indexOfCurrentSelector + 1;
     let nameOfCurrentSelector = selectsNames[selectsNames.indexOf(this.name)];
     let nameOfNextSelector = selectsNames[selectsNames.indexOf(this.name)+1];
     let url;
-    if(nameOfCurrentSelector==='region'){
-        url = urls[nameOfCurrentSelector];
-    } else {
-        url = `${urls[nameOfCurrentSelector]}${valueOfCurrentSelector}`;
-    }
-    hideSelects(this.name, selects, selectsNames);
+
+    hideNextSelects(indexOfCurrentSelector);
+    showLoadingIcon();
+    
+    url = getUrl(nameOfCurrentSelector, valueOfCurrentSelector);
+   
     setTimeout(function(){
         fetch(url)
             .then(res => res.json())
             .then(data => {
-                loadingIcon.style.display = "none";
-                if(nameOfCurrentSelector==='country'){
-                    data.map(item => {
-                        let {alpha2Code, capital, population, nativeName, flag} = item;
-                        let element = document.createElement('p');
-    
-                        element.innerText = 
-                            `Country code: ${alpha2Code}\nCapital: ${capital}
-                            Population: ${population}\nNative name: ${nativeName}`;
-                        countryInfo.appendChild(element);
-    
-                        let flagImage = document.createElement('img');
-                        flagImage.display="block";
-                        flagImage.style.width="40%";
-                        flagImage.src = flag;
-    
-                        countryInfo.appendChild(flagImage);
-                    })
-                } else{
-                    selects[indexOfCurrentSelector+1]
-                    .appendChild(createDefaultOption(nameOfNextSelector));
-            
-                // if(!data[valueOfCurrentSelector][0]) {
-                //     subregions.style.display = "none";
-                //     countries.style.display = "inline-block";
-                //     subregions.dispatchEvent(new Event('change'));
-                // } else {
-                //     selects[indexOfCurrentSelector+1].style.display = "inline-block";
-                //     //countries.style.display = "none";
-                // }
-                selects[indexOfCurrentSelector+1].style.display = "inline-block";
-                let field;
-                if(nameOfCurrentSelector==='region'){
-                    field = valueOfCurrentSelector;
-                } else {
-                    field = null;
-                }
-                let options = createOptions(data, field);
-                addOptions(selects[selectsNames.indexOf(nameOfNextSelector)], ...options);
-                }
-                      
+                hideLoadingIcon();
+                if(valueOfCurrentSelector!== 'polar' && nameOfNextSelector){
+                    addOptions(selects[indexOfNextSelector], 
+                        createDefaultOption(nameOfNextSelector));
+                    
+                    showNextSelect(indexOfNextSelector);
+                    let field;
+                    if(nameOfCurrentSelector==='region'){
+                        field = valueOfCurrentSelector;
+                    } else {
+                        field = null;
+                    }
+                    let options = createOptions(data, field);
+                    addOptions(selects[selectsNames.indexOf(nameOfNextSelector)], ...options);
+                
+                } else { 
+                    showCountryInfo(data);
+                }    
             })
-            .catch(e => {
-                console.log(e);
+            .catch(error => {
+                console.log(error);
             })
     }, 1500); 
 }
